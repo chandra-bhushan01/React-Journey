6d2fb9f8ec74655e8326935d6460f23d6df8d2bd
chandra-bhushan01 <chandrabhushanbgp321@gmail.com>
2024-04-14 18:37:07 +0530

    video 3 ended

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
+//React Element 
 const jsxHeading = (
   <h1 id="heading" className="">
     hello react using JSX
@@ -14,19 +14,29 @@ const jsxHeading = (
 
 //React Functional Components
 
-const title = () => (
+
+const elem = <span>React Element</span>
+
+
+
+const Title = (
   <h1 className="head" tabIndex="5">
     Namaste react using JSX.
   </h1>
 );
 
+
+const data = api.getData();
+
 const HeadingComponent = () => (
   <div>
-    <title />
+    {data}
     <h1 className="heading">hello fucntional component</h1>
+    {/* {Title} */}
+    <h2>{100+54}</h2>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(Title);
